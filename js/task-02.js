19d680f7317162a6464db38f52f1ff8f931f3ac0
Hanna Svietik <hsvietik@gmail.com>
2023-03-24 15:52:41 +0000

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const ingredientsList = [];
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  ingredientsList.push(item);
+});
+
+console.log(ingredientsList);
+list.append(...ingredientsList);
+
+// const list = document.querySelector("#ingredients");
+// for (const ingredient of ingredients) {
+//   const item = document.createElement("li");
+//   item.textContent = ingredient;
+//   item.classList.add("item");
+//   list.append(item);
+// }
