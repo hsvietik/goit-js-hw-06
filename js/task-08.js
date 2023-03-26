e477a3e91da67a0d1342e335de00d1600d4b00db
Hanna Svietik <hsvietik@gmail.com>
2023-03-26 18:28:27 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", formControl);
+function formControl(event) {
+  event.preventDefault();
+  if (
+    !event.target.elements.email.value ||
+    !event.target.elements.password.value
+  ) {
+    window.alert(`Bсі поля повинні бути заповнені`);
+  } else {
+    const formData = {
+      email: event.target.elements.email.value,
+      password: event.target.elements.password.value,
+    };
+    console.log(formData);
+  }
+  event.target.elements.email.value = "";
+  event.target.elements.password.value = "";
+}
