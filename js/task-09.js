d06ffcef19fa8f6e884009395c3af19e9569fa34
Hanna Svietik <hsvietik@gmail.com>
2023-03-26 18:39:17 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+changeColorBtn.addEventListener("click", changeColor);
+function changeColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.innerHTML = color;
+}
